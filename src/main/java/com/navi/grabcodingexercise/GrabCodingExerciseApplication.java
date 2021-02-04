63cd88869f9297613bacfed7e64e9dd30e90a546
Navi-nk <naval.kumar99@gmail.com>
2021-02-04 18:31:51 +0800

    added base webservice code

---
+++ src/main/java/com/navi/grabcodingexercise/GrabCodingExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.navi.grabcodingexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrabCodingExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrabCodingExerciseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/navi/grabcodingexercise/GrabCodingExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.navi.grabcodingexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrabCodingExerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
